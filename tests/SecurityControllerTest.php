5b7f098b3940f9d454cf243db31efef94e9afaea
Julien <julien.desaindes@gmail.com>
2025-04-15 11:26:51 +0200

    Merge branch 'main' of https://github.com/Judes77850/todolist into test_implement # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.

--- tests/SecurityControllerTest.php
+++ tests/SecurityControllerTest.php
@@ -4,7 +4,6 @@ namespace App\Tests;
 
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-use Symfony\Component\HttpFoundation\Response;
 
 class SecurityControllerTest extends WebTestCase
 {
